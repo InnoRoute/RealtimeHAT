78d5ec361aee4f297ae7f2745cf94edea11e1263
ulbricht-inr <ulbricht@innoroute.de>
2020-12-16 11:31:34 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Thank's for running the install script."
+echo "The setup on top of an custom raspberry-image will be available soon."
+echo "Please download a full image from https://my.hidrive.com/share/3ugldbuowd for now"
