455ead354a28b56905e76034b7c0680acdd68b33
jenkins <jenkins@innoroute.com>
2021-03-19 09:27:31 +0000

    automatic update from internal repo: 6c1696d3646611488506331d7b7d124aea7d9245

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source /usr/share/InnoRoute/hat_env.sh
 sleep 30
+MAC_addr=$(cat /sys/class/net/eth0/address)
+MAC_RT0=$(awk 'BEGIN{FS=OFS=":";$0=ARGV[1];$NF=sprintf("%X",("0x"$NF)-1);print}' $MAC_addr)
+MAC_RT2=$(awk 'BEGIN{FS=OFS=":";$0=ARGV[1];$NF=sprintf("%X",("0x"$NF)-1);print}' $MAC_RT0)
 sudo ifconfig wlan0 mtu 1500
 echo "loading userbitstream"
 /usr/share/InnoRoute/INR_write_bitstream /usr/share/InnoRoute/user_bitstream.bit 
@@ -15,6 +18,9 @@ if [ "$ID_0" -gt "0" ];then #if bitstream load successfully load driver for hat
     sleep 1
     sudo ifconfig RT0 mtu 1400
     sudo ifconfig RT2 mtu 1400
+    sudo ifconfig RT0 hw ether $MAC_RT0
+		sudo ifconfig RT2 hw ether $MAC_RT2
+    
     sudo /usr/share/InnoRoute/INR2spi $C_ADDR_SPI_INT_STATUS 0x3ff
     sleep 1
 		sudo /usr/share/InnoRoute/INR2spi $C_ADDR_SPI_INT_STATUS 0x3ff
