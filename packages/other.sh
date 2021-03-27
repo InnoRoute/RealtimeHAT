3f0caa38ef0185666a8bd1f09d32f04b9b3af8c1
jenkins <jenkins@innoroute.com>
2021-03-27 23:33:30 +0000

    automatic update from internal repo: 856e328e3160fef117c67295938044f0732c0624

--- packages/other.sh
+++ packages/other.sh
@@ -7,7 +7,7 @@ git clone https://github.com/openil/linuxptp.git
 cd linuxptp/
 git checkout OpenIL-v1.8.1-202009 
 sed -i "/\/usr\/local/cprefix	= \/usr" makefile
-sed -i "/if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, name, strlen(name))) {/cint socket_priority=7;setsockopt(fd, SOL_SOCKET, SO_PRIORITY, &socket_priority,sizeof(socket_priority));if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, name, strlen(name))) {" raw.c
+sed -i "/if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, name, strlen(name))) {/cint socket_priority=1;setsockopt(fd, SOL_SOCKET, SO_PRIORITY, &socket_priority,sizeof(socket_priority));if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, name, strlen(name))) {" raw.c
 make -j4
 sudo make install
 cd ..
